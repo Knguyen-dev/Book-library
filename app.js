3ae57039757a5d71c1ce81280b4f60c6e0cf3060
Kevin Nguyen <thenoc27@gmail.com>
2023-01-11 13:21:09 -0500

    Started working on functionality that updates the dropdown list of genres. The form's visual functionality is working and the its submission functionality is working as well. The main thing I'll be working on next will be rendering the cards for the books on screen.

--- app.js
+++ app.js
@@ -12,20 +12,115 @@ of the book in the library array
 - add a dark mode
 - have some input validation, if the book is already in the library, then don't add it
 */
+
+const numBookEl = document.getElementById("num-books");
+const numReadEl = document.getElementById("num-read");
+const numUnreadEl = document.getElementById("num-unread");
+const numGenresEl = document.getElementById("num-genres");
+
+const bookForm = document.querySelector(".book-form");
+const bookTitleEl = document.getElementById("input-title");
+const bookAuthorEl = document.getElementById("input-author");
+const numPagesEl = document.getElementById("input-pages");
+const genreEl = document.getElementById("input-genre");
+const bookReadEl = document.getElementById("input-book-read");
+
+const exitFormBtn = document.querySelector(".form-exit-btn");
+const addBookBtn = document.querySelector(".add-book-btn");
+const formModal = document.querySelector(".form-modal");
+const modalOverlay = document.querySelector(".modal-overlay");
+
+const bookGrid = document.querySelector(".book-grid");
+
+const genreDropSelect = document.getElementById("select-genre");
+
+let genresList = ["All"];
+let selectedGenre;
 let myLibrary = [];
 class Book {
-  constructor(title, author, numPages, already_read) {
+  constructor(title, author, genre, numPages, is_read) {
     this.title = title;
     this.author = author;
+    this.genre = genre;
     this.numPages = numPages;
-    this.already_read = already_read;
+    this.is_read = is_read;
   }
 }
 
-function toggleBookRead() {
-  console.log("Book read status toggled");
+exitFormBtn.addEventListener("click", hideForm);
+addBookBtn.addEventListener("click", ShowForm);
+bookForm.addEventListener("submit", addBookToLibrary);
+
+function hideForm() {
+  formModal.classList.add("content-hidden");
+  modalOverlay.classList.add("content-hidden");
+}
+function ShowForm() {
+  formModal.classList.remove("content-hidden");
+  modalOverlay.classList.remove("content-hidden");
+}
+function clearForm() {
+  bookTitleEl.value = "";
+  bookAuthorEl.value = "";
+  numPagesEl.value = null;
+  genreEl.value = "";
+  bookReadEl.checked = false;
+  console.log("Clear form triggered");
 }
 
-function addBookToLibrary() {
-  console.log("book added");
+function updateLibraryInfo() {
+  let readCount = 0;
+  let unreadCount = 0;
+
+  // Clear the genresList; genresList is only appended to
+  // so when we remove something, the genre will not be taken out of the list
+  // By resetting, the genresList can always be accurate. Always going to start and reset
+  // To "All" because selecting all genres will be an option, but the option "All" won't count as a genre such as fantasy
+  // If there are no genres we are still going to have 'all'
+  genresList = ["All"];
+
+  // loop through all books to get genres, num read, and unread
+  for (let i = 0; i < myLibrary.length; i++) {
+    const currentBook = myLibrary[i];
+    if (currentBook.is_read) {
+      readCount += 1;
+    } else {
+      unreadCount += 1;
+    }
+
+    // Update the amount of genres we have
+    if (!genresList.includes(currentBook.genre)) {
+      genresList.push(currentBook.genre);
+    }
+
+    numBookEl.textContent = myLibrary.length;
+    numReadEl.textContent = readCount;
+    numUnreadEl.textContent = unreadCount;
+    numGenresEl.textContent = genresList.length - 1; //minus one to not include 'all' option
+  }
+
+  // Update the html of the genre select dropdown
+  genreDropSelect.innerHTML = genresList
+    .map((genre) => {
+      return `<option value="${genre}">${genre}</option>`;
+    })
+    .join("");
+}
+
+function addBookToLibrary(e) {
+  e.preventDefault();
+  hideForm();
+
+  const title = bookTitleEl.value;
+  const author = bookAuthorEl.value;
+  const numPages = parseInt(numPagesEl.value);
+  const genre = genreEl.value.toLowerCase();
+  const is_read = bookReadEl.checked;
+  const newBook = new Book(title, author, genre, numPages, is_read);
+  myLibrary.push(newBook);
+
+  // Don't forget to clear form after
+  updateLibraryInfo();
+
+  clearForm();
 }
